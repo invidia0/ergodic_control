Fixed issue on rotation and clipping of the FOV

hedac_heading.py
@@ -176,7 +176,7 @@ param.dt = min(
 if param.use_fov:
     # Initialize the Field of View (FOV)
     fov_edges = utilities.init_fov(param.fov_deg, param.fov_depth)
-    # fov_edges = utilities.rotate_and_translate(fov_edges, agents[0].x, agents[0].theta)
+    fov_edges = utilities.rotate_and_translate(fov_edges, agents[0].x, agents[0].theta)
     param.kernel_size = param.fov_depth
 else:
     coverage_block = utilities.agent_block(param.nbVarX, param.min_kernel_val, param.agent_radius)
@@ -187,19 +187,22 @@ else:
 Main Loop
 ===============================
 """
+last_heading = agents[0].theta
+last_position = agents[0].x
+
 for t in range(param.nbDataPoints):
     print(f'Timestep: {t}')
 
     local_cooling = np.zeros((param.height, param.width))
     for agent in agents:
         p = agent.x
- 
         if param.use_fov:
-            fov_edges = utilities.init_fov(param.fov_deg, param.fov_depth)
-            fov_edges = utilities.rotate_and_translate(fov_edges, p, agent.theta)
+            # fov_edges = utilities.init_fov(param.fov_deg, param.fov_depth)
+            fov_edges = utilities.relative_move_fov(fov_edges, last_position, last_heading, agent.x, agent.theta)
+            # fov_edges = utilities.rotate_and_translate(fov_edges, p, agent.theta)
             # Clip the FOV
-            fov_edges = utilities.clip_polygon(fov_edges, param.box)
-            fov_points = utilities.insidepolygon(fov_edges, grid_step=param.dx).T.astype(int)
+            fov_edges_clipped = utilities.clip_polygon(fov_edges, param.box)
+            fov_points = utilities.insidepolygon(fov_edges_clipped, grid_step=param.dx).T.astype(int)
             # Delete points outside the box
             fov_points = fov_points[
                 (fov_points[:, 0] >= 0)
@@ -207,8 +210,9 @@ for t in range(param.nbDataPoints):
                 & (fov_points[:, 1] >= 0)
                 & (fov_points[:, 1] < param.height)
             ]
-            fov_probs = utilities.fov_coverage_block(fov_points, fov_edges, param.fov_depth)
-            # # # DEBUG
+            fov_probs = utilities.fov_coverage_block(fov_points, fov_edges_clipped, param.fov_depth)
+
+            # # DEBUG
             # fig = plt.figure()
             # ax = fig.add_subplot(111)
             # ax.scatter(p[0], p[1], c='black', s=100, marker='o')
@@ -222,6 +226,7 @@ for t in range(param.nbDataPoints):
             # ax.set_aspect('equal')
             # ax.set_title('Field of View')
             # plt.show()
+
             coverage_density[fov_points[:, 1], fov_points[:, 0]] += fov_probs
         else:
             adjusted_position = p / param.dx
@@ -280,6 +285,9 @@ for t in range(param.nbDataPoints):
     gradient_x = gradient_x / np.linalg.norm(gradient_x)
     gradient_y = gradient_y / np.linalg.norm(gradient_y)
 
+    last_heading = agents[0].theta
+    last_position = agents[0].x
+
     for agent in agents:
         grad = utilities.calculate_gradient(
             param,
@@ -295,13 +303,21 @@ for t in range(param.nbDataPoints):
         ax[1].cla()
 
         ax[0].contourf(grids_x*100, grids_y*100, goal_density, cmap='Reds', levels=10)
-        ax[0].plot(agents[0].x_hist[:, 0], agents[0].x_hist[:, 1], c='black', alpha=1, lw=2)
+        # ax[0].plot(agents[0].x_hist[:, 0], agents[0].x_hist[:, 1], c='black', alpha=1, lw=2)
+        time_array = np.linspace(0, param.nbDataPoints, param.nbDataPoints)
+        lines = utilities.colored_line(agents[0].x_hist[:, 0], 
+                                        agents[0].x_hist[:, 1],
+                                        time_array,
+                                        ax[0],
+                                        cmap='Greys',
+                                        linewidth=2)
         # FOV
         if param.use_fov:
             ax[0].fill(fov_edges[:, 0], fov_edges[:, 1], 'b', alpha=0.2)
         # Heading
-        ax[0].quiver(agents[0].x[0], agents[0].x[1], np.cos(agents[0].theta), np.sin(agents[0].theta), scale = 2, scale_units='inches')
-        ax[0].scatter(agents[0].x[0], agents[0].x[1], c='tab:blue', s=100, marker='o')
+        ax[0].quiver(agents[0].x[0], agents[0].x[1], np.cos(agents[0].theta), np.sin(agents[0].theta), scale = None, scale_units='inches')
+        ax[0].scatter(agents[0].x_hist[0, 0], agents[0].x_hist[0, 1], s=100, facecolors='none', edgecolors='black', lw=2)
+        ax[0].scatter(agents[0].x[0], agents[0].x[1], c='k', s=100, marker='o', zorder=10)
 
         ax[1].contourf(grids_x*100, grids_y*100, heat, cmap='coolwarm', levels=10)
         ax[1].quiver(grids_x*100, grids_y*100, gradient_x / np.linalg.norm(gradient_x), gradient_y / np.linalg.norm(gradient_y), scale = 1, scale_units='inches')